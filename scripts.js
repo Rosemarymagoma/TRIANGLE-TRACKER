12d969453710e7cadab1a331eb1b16917ed28c9d
rosemarymagoma99@gmail.com <rosemarymagoma99@gmail.com>
2018-09-14 15:18:35 +0300

    Copy the triangle calculations from the js fiddle to script.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var lengthA = "A value";
+var lengthB ="B value"
+var lenthC = "C value";
+var triangleType = ["Equilateral", "Isosceles", "Scalene"];
+
+if (lengthA==lengthB&&lengthB==lengthC&&lengthC==lengthA) {
+    console.log("Equilateral");
+} else if (lengthA==lengthB||lengthB==lengthC||lengthC==length) {
+    console.log("Isosceles");
+} else if (lengthA!==lengthB&&lengthB!==lengthC&&lengthC!==lengthA) {
+    console.log("scalene");
+} else {
+    console.log("Not a triangle");
+}

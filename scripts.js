55867252e8ce7b92e75207a9382d2c18e7ec4d76
Rosemarymagoma <42939349+Rosemarymagoma@users.noreply.github.com>
2018-09-14 22:03:01 +0300

    script.js
    Add outputs.innerHTML in the script

--- scripts.js
+++ scripts.js
@@ -5,14 +5,15 @@ var lengthA = parseInt(document.getElementById('a').value);
 var lengthB =parseInt(document.getElementById('b').value);
 var lengthC = parseInt(document.getElementById('c').value);
 var outputs = document.getElementById("outputs");
-if (lengthA==lengthB&&lengthB==lengthC&&lengthC==lengthA) {
+    if (lengthA>=(lengthB+lengthC)||lengthB>=(lengthA+lengthC)||lengthC>=(lengthA+lengthB) {
+        outputs.innerHTML("Not a triangle");
+} else if (lengthA==lengthB&&lengthB==lengthC&&lengthC==lengthA) {
     outputs.innerHTML ="Equilateral";
 } else if (lengthA==lengthB||lengthB==lengthC||lengthC==length) {
-    console.log("Isosceles");
-} else if (lengthA!==lengthB&&lengthB!==lengthC&&lengthC!==lengthA) {
-    console.log("scalene");
-} else {
-    console.log("Not a triangle");
-}
+    outputs.innerHTML("Isosceles");
+} else  (lengthA!==lengthB&&lengthB!==lengthC&&lengthC!==lengthA) {
+     outputs.innerHTML("scalene");
+} 
+
 
 }

a567a40c4a4407813a2991a63441fb459947f667
rosemarymagoma99@gmail.com <rosemarymagoma99@gmail.com>
2018-09-15 17:08:56 +0300

    Add an id triangle

--- scripts.js
+++ scripts.js
@@ -2,17 +2,17 @@
 function triangleTracker() {
 
 var lengthA = parseInt(document.getElementById('a').value);
-var lengthB =parseInt(document.getElementById('b').value);
+var lengthB = parseInt(document.getElementById('b').value);
 var lengthC = parseInt(document.getElementById('c').value);
 var outputs = document.getElementById("outputs");
-if (lengthA==lengthB&&lengthB==lengthC&&lengthC==lengthA) {
-    outputs.textContent ="Equilateral";
-} else if (lengthA==lengthB||lengthB==lengthC||lengthC==length) {
+if(lengthA >= (lengthB+lengthC) || lengthB >= (lengthA+lengthC) || lengthC >= (lengthB+lengthA)) {
+  outputs.textContext="Not a triangle";
+} else if (lengthA == lengthB && lengthB== lengthC && lengthC == lengthA) {
+    console.log("Equilateral");
+} else if (lengthA == lengthB || lengthB == lengthC || lengthC == lengthA) {
     console.log("Isosceles");
-} else if (lengthA!==lengthB&&lengthB!==lengthC&&lengthC!==lengthA) {
-    console.log("scalene");
-} else {
-    console.log("Not a triangle");
+} else (lengthA !== lengthB && lengthB !== lengthC) {
+    console.log("Scalene");
 }
 
 }
